bb824ebdf59fc4b6df8e8ec240e9672a9d11ef42
JCH393 <jch11@kent.ac.uk>
2026-02-08 14:24:09 +0000

    display_menu

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,3 +5,17 @@ def init_database():
     ids = [1001,1002,1003,1004,1005]
     return names, ranks, divs, ids
 
+def display_menu():
+    student_name = input('What is your full name? ')
+
+    print("\n--- MENU ---")
+    print("Student logged in:", student_name)
+    print("1. View Crew")
+    print("2. Add Crew")
+    print("3. Remove Crew")
+    print("4. Analyze Data")
+    print("5. Exit")
+    
+    choice = input("Select option: ").strip()
+    return choice
+
